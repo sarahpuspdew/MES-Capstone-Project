5d942a4ce5d9ee6214e91dbe06164fcce52556be
sarahpuspdew <sarahpuspadewi.spd@gmail.com>
2022-11-28 20:42:26 +0700

    Added project

---
+++ MOS-ACP Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+}
---
+++ MOS-ACP Game/Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public static MenuManager Instance;
+
+    public void QuitGame()
+    {
+        #if UNITY_EDITOR
+            EditorApplication.ExitPlaymode();
+        #else
+            Application.Quit();
+        #endif
+    }
+}
