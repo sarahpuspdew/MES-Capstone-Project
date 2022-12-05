efcd8cdfa4fa849395d671fc4dc7fdc273c061c0
sarahpuspdew <sarahpuspadewi.spd@gmail.com>
2022-12-05 10:08:03 +0700

    Add link regris for level 6

---
+++ MOS-ACP Game/Assets/Scripts/OpenLink.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink : MonoBehaviour
+{
+    public void regrisACP()
+    {
+        Application.OpenURL("https://myedusolve.pro.typeform.com/to/ZtC8cbnR?typeform-source=myedusolve.com");
+    }
+
+    public void regrisMOS()
+    {
+        Application.OpenURL("https://myedusolve.pro.typeform.com/to/C0wJmH2I?typeform-source=myedusolve.com");
+    }
+}
